a50a4ba0e514dc056602eb176942cc5962161835
Vishnu Pratap Singh <vishnupratap898@gmail.com>
2024-12-26 12:48:13 +0530

    create server and uitlities

--- src/database/index.js
+++ src/database/index.js
@@ -3,13 +3,8 @@ import { DB_NAME } from "../constants.js";
 
 export default async function connectDB() {
     try {
-        const connectionInstance = await mongoose.connect(
-            `${process.env.MONGODB_URI}/${DB_NAME}`
-        );
-        console.log(
-            "MongoDB Connected!! DB Host: ",
-            connectionInstance.connections[0].host
-        );
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log("MongoDB Connected!!");
     } catch (error) {
         console.log("MongoDB Connection Error: ", error);
     }
--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
 import connectDB from "./database/index.js";
-import express from "express";
+import { app } from "./app";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-connectDB();
+const port = process.env.PORT || 4000;
+
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+
+        app.on("error", (err) => {
+            console.log(`Server error: ${err}`);
+            throw err;
+        });
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
