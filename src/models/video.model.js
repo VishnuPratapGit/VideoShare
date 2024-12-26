dbb8f6f371b79ebd6a833603c43baae75e391e0a
Vishnu Pratap Singh <vishnupratap898@gmail.com>
2024-12-26 21:35:24 +0530

    create user and video modle

---
+++ src/models/video.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const videoSchema = mongoose.Schema(
+    {
+        videoFile: {
+            type: String,
+            required: true,
+        },
+
+        thumbnail: {
+            type: String,
+            required: true,
+        },
+
+        title: {
+            type: String,
+            default: Date.now(),
+        },
+
+        description: {
+            type: String,
+            default: "no description availiable",
+        },
+
+        duration: {
+            type: Number,
+            required: true,
+        },
+
+        views: {
+            type: Number,
+            default: 0,
+        },
+
+        isPublished: {
+            type: Boolean,
+            default: false,
+        },
+
+        owner: {
+            type: mongoose.Schema.type.ObjectId,
+            ref: "User",
+        },
+    },
+    { timestamps: true }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
