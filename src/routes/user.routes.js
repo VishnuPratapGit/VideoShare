636ea203e7dd90a9e8c41b9a4cc78e8c2c31657c
Vishnu Pratap Singh <vishnupratap898@gmail.com>
2025-01-02 16:49:35 +0530

    add routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,19 @@
 import { Router } from "express";
+import { uploadMulter } from "../middlewares/multer.middleware.js";
+import verifyJWT from "../middlewares/auth.middleware.js";
 import {
     loginUser,
     registerUser,
     logoutUser,
     refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateUserDetails,
+    updateAvatar,
+    updateCoverImage,
+    getUserChannelProfile,
+    getHistory,
 } from "../controllers/user.controller.js";
-import { uploadMulter } from "../middlewares/multer.middleware.js";
-import verifyJWT from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
@@ -17,9 +24,20 @@ userRouter.route("/register").post(
     ]),
     registerUser
 );
-
 userRouter.route("/login").post(loginUser);
 userRouter.route("/logout").post(verifyJWT, logoutUser);
 userRouter.route("/refresh-token").post(refreshAccessToken);
+userRouter.route("/change-password").post(verifyJWT, changeCurrentPassword);
+userRouter.route("/getuser").get(verifyJWT, getCurrentUser);
+userRouter.route("/update-details").patch(verifyJWT, updateUserDetails);
+userRouter
+    .route("/update-avatar")
+    .patch(verifyJWT, uploadMulter.single("avatar"), updateAvatar);
+
+userRouter
+    .route("/update-coverimage")
+    .patch(verifyJWT, uploadMulter.single("coverImage"), updateCoverImage);
+userRouter.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+userRouter.route("/history").get(verifyJWT, getHistory);
 
 export default userRouter;
